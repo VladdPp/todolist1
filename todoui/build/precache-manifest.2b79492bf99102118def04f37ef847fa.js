9a38efb68d0760c058a5a0924bcb3356804123ef
VladdPp <vladd.plat@gmail.com>
2021-11-07 23:33:26 +0200

    update

---
+++ todoui/build/precache-manifest.2b79492bf99102118def04f37ef847fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3490483fec9e9f21cce03038e29dd90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58ed0e4d54cd13b26e90",
+    "url": "/static/css/main.66f5ef43.chunk.css"
+  },
+  {
+    "revision": "f926f64ec7d3bcd916cd",
+    "url": "/static/js/2.42cfc1a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.42cfc1a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ed0e4d54cd13b26e90",
+    "url": "/static/js/main.7df4fb89.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
